63416da3973b9fa62386de54564257e4bfb264bc
Simone Herak <sherak@riteh.hr>
2018-11-07 13:32:53 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='App which monitors fridge status.',
+    author='Simone Herak',
+    license='MIT',
+)
